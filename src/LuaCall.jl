7f372bf9a3365c2fde324344cd477468eb62858d
raphasampaio <raphaelaraujosampaio@gmail.com>
2025-02-26 00:09:14 -0300

    Initial commit

--- src/LuaCall.jl
+++ src/LuaCall.jl
@@ -1,48 +1,32 @@
 module LuaCall
 
-export from_lua, to_lua, @lua
+export from_lua, to_lua, @lua, @push_lua_function
 
 include("capi.jl")
 import .C
 
 include("state.jl")
 
-macro lua(fndef)
-    # Ensure the macro is applied to a function definition.
-    if fndef.head != :function
-        error("@lua macro must be applied to a function definition")
-    end
+macro lua(func_def)
+    function_name = func_def.args[1].args[1]
 
-    # Extract the function name.
-    local fn_name_sym = if isa(fndef.args[1], Symbol)
-        fndef.args[1]
-    elseif isa(fndef.args[1], Expr) && fndef.args[1].head == :call
-        fndef.args[1].args[1]
-    else
-        error("@lua: invalid function signature")
-    end
+    return esc(quote
+        $func_def
 
-    # Create a name for the Lua callback wrapper.
-    local wrapper_name = Symbol(string(fn_name_sym) * "_lua")
-
-    # Build an expression for the wrapper definition.
-    local wrapper_expr = quote
-        function $(QuoteNode(wrapper_name))(L::Ptr{Cvoid})::Cint
-            local args = LuaCall.from_lua(L)
-            local result = $(esc(fn_name_sym))(args...)
+        function $function_name(L::Ptr{Cvoid})::Cint
+            args = LuaCall.from_lua(L)
+            result = $function_name(args...)
             return LuaCall.to_lua(L, result)
         end
-    end
-
-    return quote
-        # Define the original function normally.
-        $(esc(fndef))
-        # At runtime, inject the Lua callback wrapper into Main.
-        Core.eval(Main, $(QuoteNode(wrapper_expr)))
-    end
+    end)
 end
 
-
-
+macro push_lua_function(L, lua_function, julia_function)
+    return esc(quote
+        f = @cfunction($julia_function, Cint, (Ptr{Cvoid},))
+        LuaCall.C.lua_pushcfunction($L, f)
+        LuaCall.C.lua_setglobal($L, $lua_function)
+    end)
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,63 +1,121 @@
 using LuaCall
 
-using Aqua
-using Test
+# using Aqua
+# using Test
 
-function mysin(L::Ptr{Cvoid})::Cint
-    d = LuaCall.C.lua_tonumber(L, 1)
-    LuaCall.C.lua_pushnumber(L, sin(d))
-    return 1
-end
+# function mysin(L::Ptr{Cvoid})::Cint
+#     d = LuaCall.C.lua_tonumber(L, 1)
+#     LuaCall.C.lua_pushnumber(L, sin(d))
+#     return 1
+# end
 
-function test_capi()
-    L = LuaCall.C.luaL_newstate()
-    LuaCall.C.luaL_openlibs(L)
+# function test_capi()
+#     L = LuaCall.C.luaL_newstate()
+#     LuaCall.C.luaL_openlibs(L)
 
-    LuaCall.C.lua_pushcfunction(L, @cfunction(mysin, Cint, (Ptr{Cvoid},)))
-    LuaCall.C.lua_setglobal(L, "mysin")
+#     LuaCall.C.lua_pushcfunction(L, @cfunction(mysin, Cint, (Ptr{Cvoid},)))
+#     LuaCall.C.lua_setglobal(L, "mysin")
 
-    LuaCall.C.lua_getglobal(L, "mysin")
-    LuaCall.C.lua_pushnumber(L, 1)
+#     LuaCall.C.lua_getglobal(L, "mysin")
+#     LuaCall.C.lua_pushnumber(L, 1)
 
-    if LuaCall.C.lua_pcallk(L, 1, 1, 0, 0, C_NULL) != 0
-        error("Error calling mysin: ", LuaCall.C.lua_tostring(L, -1))
-    end
+#     if LuaCall.C.lua_pcallk(L, 1, 1, 0, 0, C_NULL) != 0
+#         error("Error calling mysin: ", LuaCall.C.lua_tostring(L, -1))
+#     end
 
-    result = LuaCall.C.lua_tonumber(L, -1)
+#     result = LuaCall.C.lua_tonumber(L, -1)
 
-    @test result ≈ sin(1)
+#     @test result ≈ sin(1)
 
-    LuaCall.C.lua_close(L)
+#     LuaCall.C.lua_close(L)
 
-    return nothing
-end
+#     return nothing
+# end
 
-function print_lua_argument_types(L::Ptr{Cvoid})::Cint
-    num_args = LuaCall.C.lua_gettop(L)
-    println("Number of arguments on the stack: ", num_args)
+# function print_lua_argument_types(L::Ptr{Cvoid})::Cint
+#     num_args = LuaCall.C.lua_gettop(L)
+#     println("Number of arguments on the stack: ", num_args)
 
-    for i in 1:num_args
-        arg_type = LuaCall.C.lua_type(L, i)
-        type_name = unsafe_string(LuaCall.C.lua_typename(L, arg_type))
-        println("Argument ", i, " type: ", type_name)
-    end
+#     for i in 1:num_args
+#         arg_type = LuaCall.C.lua_type(L, i)
+#         type_name = unsafe_string(LuaCall.C.lua_typename(L, arg_type))
+#         println("Argument ", i, " type: ", type_name)
+#     end
 
-    return 0
-end
+#     return 0
+# end
 
-function test_print_lua_argument_types()
-    L = LuaCall.C.luaL_newstate()
-    LuaCall.C.luaL_openlibs(L)
+# function test_print_lua_argument_types()
+#     L = LuaCall.C.luaL_newstate()
+#     LuaCall.C.luaL_openlibs(L)
 
-    LuaCall.C.lua_pushcfunction(L, @cfunction(print_lua_argument_types, Cint, (Ptr{Cvoid},)))
-    LuaCall.C.lua_setglobal(L, "print_lua_argument_types")
+#     LuaCall.C.lua_pushcfunction(L, @cfunction(print_lua_argument_types, Cint, (Ptr{Cvoid},)))
+#     LuaCall.C.lua_setglobal(L, "print_lua_argument_types")
 
-    LuaCall.safe_script(L, "print_lua_argument_types(1, 'hello', true)")
+#     LuaCall.safe_script(L, "print_lua_argument_types(1, 'hello', true)")
 
-    LuaCall.C.lua_close(L)
+#     LuaCall.C.lua_close(L)
 
-    return nothing
-end
+#     return nothing
+# end
+
+# # function call_julia_from_lua(L::Ptr{Cvoid})::Cint
+# #     @show args = LuaCall.from_lua(L)
+
+# #     result = julia_function(args...)
+
+# #     return LuaCall.to_lua(L, result)
+# # end
+
+# function test_call_julia_from_lua()
+#     L = LuaCall.C.luaL_newstate()
+#     LuaCall.C.luaL_openlibs(L)
+
+#     @show julia_function_lua
+
+#     # LuaCall.C.lua_pushcfunction(L, @cfunction(julia_function_lua, Cint, (Ptr{Cvoid},)))
+#     # LuaCall.C.lua_setglobal(L, "call_julia_from_lua")
+
+#     # LuaCall.safe_script(L, """
+#     #     print("Calling julia_function with a number:")
+#     #     print(call_julia_from_lua(42.0))
+
+#     #     print("Calling julia_function with a string:")
+#     #     print(call_julia_from_lua("A string from Lua"))
+
+#     #     print("Calling julia_function with two numbers:")
+#     #     print(call_julia_from_lua(1.0, 2.0))
+#     # """)
+
+#     LuaCall.C.lua_close(L)
+
+#     return nothing
+# end
+
+# function test_all()
+#     # @testset "C API" begin
+#     #     test_capi()
+#     # end
+
+#     # test_print_lua_argument_types()
+
+#     test_call_julia_from_lua()
+
+#     # L = LuaCall.C.luaL_newstate()
+
+#     # LuaCall.C.luaL_openlibs(L)
+
+#     # LuaCall.C.lua_pushcfunction(L, @cfunction(l_sin, Cint, (Ptr{Cvoid},)))
+#     # LuaCall.C.lua_setglobal(L, "mysin")
+
+#     # LuaCall.safe_script(L, "print(mysin(1))")
+
+#     # LuaCall.C.lua_close(L)
+
+#     return nothing
+# end
+
+# test_all()
 
 @lua function julia_function(a::Float64)
     println("Called julia_function with a Float64: ", a)
@@ -74,60 +132,34 @@ end
     return a + b
 end
 
-# function call_julia_from_lua(L::Ptr{Cvoid})::Cint
-#     @show args = LuaCall.from_lua(L)
-   
-#     result = julia_function(args...)
+macro push_lua_function(L, lua_function, julia_function)
+    return esc(quote
+        f = @cfunction($julia_function, Cint, (Ptr{Cvoid},))
+        LuaCall.C.lua_pushcfunction($L, f)
+        LuaCall.C.lua_setglobal($L, $lua_function)
+    end)
+end
 
-#     return LuaCall.to_lua(L, result)
-# end
 
-function test_call_julia_from_lua()
+function test_print_lua_argument_types()
     L = LuaCall.C.luaL_newstate()
     LuaCall.C.luaL_openlibs(L)
 
-    @show julia_function_lua
+    # @show methods(julia_function)
 
-    # LuaCall.C.lua_pushcfunction(L, @cfunction(julia_function_lua, Cint, (Ptr{Cvoid},)))
-    # LuaCall.C.lua_setglobal(L, "call_julia_from_lua")
+    # f = @cfunction(julia_function, Cint, (Ptr{Cvoid},))
+    # LuaCall.C.lua_pushcfunction(L, f)
+    # LuaCall.C.lua_setglobal(L, "print_lua_argument_types")
 
-    # LuaCall.safe_script(L, """
-    #     print("Calling julia_function with a number:")
-    #     print(call_julia_from_lua(42.0))
+    @push_lua_function(L, "print_lua_argument_types", julia_function)
 
-    #     print("Calling julia_function with a string:")
-    #     print(call_julia_from_lua("A string from Lua"))
-
-    #     print("Calling julia_function with two numbers:")
-    #     print(call_julia_from_lua(1.0, 2.0))
-    # """)
+    LuaCall.safe_script(L, "print_lua_argument_types(1)")
+    LuaCall.safe_script(L, "print_lua_argument_types(\"aaa\")")
+    LuaCall.safe_script(L, "print_lua_argument_types(1, 2)")
 
     LuaCall.C.lua_close(L)
 
     return nothing
 end
 
-function test_all()
-    # @testset "C API" begin
-    #     test_capi()
-    # end
-
-    # test_print_lua_argument_types()
-
-    test_call_julia_from_lua()
-
-    # L = LuaCall.C.luaL_newstate()
-
-    # LuaCall.C.luaL_openlibs(L)
-
-    # LuaCall.C.lua_pushcfunction(L, @cfunction(l_sin, Cint, (Ptr{Cvoid},)))
-    # LuaCall.C.lua_setglobal(L, "mysin")
-
-    # LuaCall.safe_script(L, "print(mysin(1))")
-
-    # LuaCall.C.lua_close(L)
-
-    return nothing
-end
-
-test_all()
+test_print_lua_argument_types()
