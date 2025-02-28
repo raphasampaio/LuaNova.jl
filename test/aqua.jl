b11ebc300f99f3f4ab07a0a798d46782ec682995
raphasampaio <raphaelaraujosampaio@gmail.com>
2025-02-28 18:44:38 -0300

    Initial commit

--- test/aqua.jl
+++ test/aqua.jl
@@ -1,8 +1,8 @@
 function test_aqua()
     @testset "Ambiguities" begin
-        Aqua.test_ambiguities(Lua, recursive = false)
+        Aqua.test_ambiguities(LuaNova, recursive = false)
     end
-    Aqua.test_all(Lua, ambiguities = false)
+    Aqua.test_all(LuaNova, ambiguities = false)
 
     return nothing
 end
