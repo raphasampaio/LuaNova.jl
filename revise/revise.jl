cf60ba3286f7cdabc11214175cd47c776e65bba8
raphasampaio <raphaelaraujosampaio@gmail.com>
2025-02-28 18:43:58 -0300

    Initial commit

--- revise/revise.jl
+++ revise/revise.jl
@@ -6,8 +6,8 @@ using Revise
 Pkg.activate(dirname(@__DIR__))
 Pkg.instantiate()
 
-using Lua
+using LuaNova
 @info("""
-This session is using Lua.jl with Revise.jl.
+This session is using LuaNova.jl with Revise.jl.
 For more information visit https://timholy.github.io/Revise.jl/stable/.
 """)
