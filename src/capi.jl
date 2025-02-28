412e66be83ebdd01fdbe8d5d2b24c69b1c5a2ef4
raphasampaio <raphaelaraujosampaio@gmail.com>
2025-02-28 18:51:33 -0300

    Initial commit

--- src/capi.jl
+++ src/capi.jl
@@ -5,6 +5,7 @@ export Lua_jll
 
 const UINT_MAX = typemax(Int64)
 
+
 const lua_Integer = Clonglong
 
 const lua_Number = Cdouble
@@ -108,7 +109,7 @@ function Base.getproperty(x::var"union (unnamed at C:\\Users\\rsampaio\\.julia\\
 end
 
 function Base.setproperty!(x::Ptr{var"union (unnamed at C:\\Users\\rsampaio\\.julia\\artifacts\\38e43295d4913c3d9e6b8baf05f545a89880759c\\include\\lauxlib.h:196:3)"}, f::Symbol, v)
-    return unsafe_store!(getproperty(x, f), v)
+    unsafe_store!(getproperty(x, f), v)
 end
 
 struct luaL_Buffer
@@ -132,7 +133,7 @@ function Base.getproperty(x::luaL_Buffer, f::Symbol)
 end
 
 function Base.setproperty!(x::Ptr{luaL_Buffer}, f::Symbol, v)
-    return unsafe_store!(getproperty(x, f), v)
+    unsafe_store!(getproperty(x, f), v)
 end
 
 function luaL_prepbuffsize(B, sz)
@@ -957,10 +958,40 @@ const LUA_DBLIBNAME = "debug"
 
 const LUA_LOADLIBNAME = "package"
 
+#define lua_getextraspace(L)	((void *)((char *)(L) - LUA_EXTRASPACE))
+
 lua_tonumber(L, i) = lua_tonumberx(L, (i), C_NULL)
+#define lua_tointeger(L,i)	lua_tointegerx(L,(i),NULL)
+
+#define lua_pop(L,n)		lua_settop(L, -(n)-1)
+
+#define lua_newtable(L)		lua_createtable(L, 0, 0)
+
+#define lua_register(L,n,f) (lua_pushcfunction(L, (f)), lua_setglobal(L, (n)))
 
 lua_pushcfunction(L, f) = lua_pushcclosure(L, (f), 0)
 
+#define lua_isfunction(L,n)	(lua_type(L, (n)) == LUA_TFUNCTION)
+#define lua_istable(L,n)	(lua_type(L, (n)) == LUA_TTABLE)
+#define lua_islightuserdata(L,n)	(lua_type(L, (n)) == LUA_TLIGHTUSERDATA)
+#define lua_isnil(L,n)		(lua_type(L, (n)) == LUA_TNIL)
+#define lua_isboolean(L,n)	(lua_type(L, (n)) == LUA_TBOOLEAN)
+#define lua_isthread(L,n)	(lua_type(L, (n)) == LUA_TTHREAD)
+#define lua_isnone(L,n)		(lua_type(L, (n)) == LUA_TNONE)
+#define lua_isnoneornil(L, n)	(lua_type(L, (n)) <= 0)
+
+#define lua_pushliteral(L, s)	lua_pushstring(L, "" s)
+
+#define lua_pushglobaltable(L)  \
+# ((void)lua_rawgeti(L, LUA_REGISTRYINDEX, LUA_RIDX_GLOBALS))
+
 lua_tostring(L, i) = lua_tolstring(L, (i), C_NULL)
+
+#define lua_insert(L,idx)	lua_rotate(L, (idx), 1)
+
+#define lua_remove(L,idx)	(lua_rotate(L, (idx), -1), lua_pop(L, 1))
+
+#define lua_replace(L,idx)	(lua_copy(L, -1, (idx)), lua_pop(L, 1))
+
 
 end # module
