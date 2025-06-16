98f129392011690fce3a931bbf5ac8bfdcb361d3
raphasampaio <raphaelaraujosampaio@gmail.com>
2025-06-16 00:28:55 -0300

    Initial commit

--- src/macros.jl
+++ src/macros.jl
@@ -1,30 +1,3 @@
-function push_lua_value(L::Ptr{Cvoid}, value)
-    if value isa Number
-        C.lua_pushnumber(L, value)
-    elseif value isa String
-        C.lua_pushstring(L, value)
-    elseif value isa Ptr
-        C.lua_pushlightuserdata(L, value)
-    else
-        error("Unsupported value type")
-    end
-end
-
-function pop_lua_value(L::Ptr{Cvoid}, index::Integer)
-    C.luaL_checktype(L, index, Lua.TANY)
-    t = C.lua_type(L, index)
-
-    if t == Lua.LUA_TNUMBER
-        return C.lua_tonumber(L, index)
-    elseif t == Lua.LUA_TSTRING
-        return C.lua_tostring(L, index)
-    elseif t == Lua.LUA_TLIGHTUSERDATA
-        return C.lua_touserdata(L, index)
-    else
-        error("Unsupported Lua value type")
-    end
-end
-
 macro define_lua_function(function_name)
     return esc(quote
         function $function_name(L::Ptr{Cvoid})::Cint
@@ -39,7 +12,7 @@ end
 macro push_lua_function(L, lua_function, julia_function)
     return esc(quote
         f = @cfunction($julia_function, Cint, (Ptr{Cvoid},))
-        LuaNova.C.lua_pushcfunction($L, f)
+        LuaNova.push_cfunction($L, f)
         LuaNova.set_global($L, $lua_function)
     end)
 end
