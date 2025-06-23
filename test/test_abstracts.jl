b6afa3b8970f9ad41f3b2a828059a47df796d396
raphasampaio <raphaelaraujosampaio@gmail.com>
2025-06-23 00:46:48 -0300

    Update

---
+++ test/test_abstracts.jl
@@ -0,0 +1,53 @@
+module TestAbstracts
+
+using LuaNova
+using Test
+
+abstract type Vehicle end
+
+mutable struct Car <: Vehicle end
+@define_lua_struct Car
+
+mutable struct Truck <: Vehicle end
+@define_lua_struct Truck
+
+function move(::Vehicle)
+    return "not defined"
+end
+
+function move(::Car)
+    return "defined"
+end
+
+@define_lua_function move
+
+@testset "Abstracts" begin
+    L = LuaNova.new_state()
+    LuaNova.open_libs(L)
+
+    @push_lua_struct(
+        L, Car,
+        "move", move,
+    )
+
+    @push_lua_struct(
+        L, Truck,
+        "move", move,
+    )
+
+    LuaNova.safe_script(L, "c = Car()")
+    LuaNova.safe_script(L, "return c:move()")
+    result = LuaNova.to_string(L, -1)
+    @test result == "defined"
+
+    LuaNova.safe_script(L, "t = Truck()")
+    LuaNova.safe_script(L, "return t:move()")
+    result = LuaNova.to_string(L, -1)
+    @test result == "not defined"
+
+    LuaNova.close(L)
+
+    return nothing
+end
+
+end
