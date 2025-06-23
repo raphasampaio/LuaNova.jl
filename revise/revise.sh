58140c5c39fec811264c0d08a1103c4310fdfc7f
raphasampaio <raphaelaraujosampaio@gmail.com>
2025-06-22 23:12:31 -0300

    [skip ci] update

--- revise/revise.sh
+++ revise/revise.sh
@@ -2,4 +2,4 @@
 
 BASEPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-$JULIA_1113 --project=$BASEPATH --interactive --load=$BASEPATH/revise.jl
+$JULIA_1115 --project=$BASEPATH --interactive --load=$BASEPATH/revise.jl
--- test/test.sh
+++ test/test.sh
@@ -2,4 +2,4 @@
 
 BASEPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-$JULIA_1113 --project=$BASEPATH/.. -e "import Pkg; Pkg.test()"
+$JULIA_1115 --project=$BASEPATH/.. -e "import Pkg; Pkg.test()"
