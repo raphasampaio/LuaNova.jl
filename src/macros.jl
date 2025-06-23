9e5d5abb736906d1b8079df214555845fe60749d
raphasampaio <raphaelaraujosampaio@gmail.com>
2025-06-22 23:57:13 -0300

    Update

--- src/macros.jl
+++ src/macros.jl
@@ -80,7 +80,6 @@ macro push_lua_struct(L::Symbol, julia_struct::Symbol, args...)
         LuaNova.set_functions($L, methods)
         LuaNova.lua_pop!($L, 1)
 
-        # constructor
         LuaNova.push_cfunction($L, @cfunction($julia_struct, Cint, (Ptr{LuaNova.C.lua_State},)))
         LuaNova.set_global($L, $struct_string)
     end)
