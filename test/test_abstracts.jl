4f5fadcdd41e249024b7d52377fc48c411e86134
raphasampaio <raphaelaraujosampaio@gmail.com>
2025-06-26 12:10:44 -0300

    Update

--- test/test_abstracts.jl
+++ test/test_abstracts.jl
@@ -15,7 +15,7 @@ move(::Vehicle) = "not defined"
 move(::Car) = "defined"
 @define_lua_function move
 
-@testset "Abstracts" begin
+@testset "Abstracts 1" begin
     L = LuaNova.new_state()
     LuaNova.open_libs(L)
 
@@ -31,6 +31,49 @@ move(::Car) = "defined"
     LuaNova.safe_script(L, "return t:move()")
     result = LuaNova.to_string(L, -1)
     @test result == "not defined"
+
+    LuaNova.close(L)
+
+    return nothing
+end
+
+abstract type Expression end
+
+function save(::Expression)
+    return true
+end
+@define_lua_function save
+
+mutable struct Data <: Expression end
+@define_lua_struct Data
+
+mutable struct Binary <: Expression
+    left::Expression
+    right::Expression
+end
+
+abstract type Collection end
+
+mutable struct Thermal <: Collection end
+@define_lua_struct Thermal
+
+function load(::Thermal)
+    return Data()
+end
+@define_lua_function load
+
+@testset "Abstracts 2" begin
+    L = LuaNova.new_state()
+    LuaNova.open_libs(L)
+
+    @push_lua_struct(L, Data, "save", save)
+    @push_lua_struct(L, Thermal, "load", load)
+
+    LuaNova.safe_script(L, "thermal = Thermal()")
+    LuaNova.safe_script(L, "exp = thermal:load()")
+    LuaNova.safe_script(L, "return exp:save()")
+    result = LuaNova.to_boolean(L, -1)
+    @test result == true
 
     LuaNova.close(L)
 
