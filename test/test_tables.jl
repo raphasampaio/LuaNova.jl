2a6914c2baaefedf03ab20712fd1a2457e5173e4
raphasampaio <raphaelaraujosampaio@gmail.com>
2025-08-27 23:53:29 -0300

    Update

--- test/test_tables.jl
+++ test/test_tables.jl
@@ -23,34 +23,34 @@ using Test
     LuaNova.open_libs(L)
 
     LuaNova.safe_script(L, "t = {\"a\", \"b\", \"c\"};")
-    
+
     LuaNova.get_global(L, "t")
     @test LuaNova.is_table(L, -1)
-    
+
     LuaNova.push_to_lua!(L, 1)
     LuaNova.get_table(L, -2)
     result1 = LuaNova.to_string(L, -1)
     @test result1 == "a"
     LuaNova.lua_pop!(L, 1)
-    
+
     LuaNova.push_to_lua!(L, 2)
     LuaNova.get_table(L, -2)
     result2 = LuaNova.to_string(L, -1)
     @test result2 == "b"
     LuaNova.lua_pop!(L, 1)
-    
+
     LuaNova.push_to_lua!(L, 3)
     LuaNova.get_table(L, -2)
     result3 = LuaNova.to_string(L, -1)
     @test result3 == "c"
     LuaNova.lua_pop!(L, 1)
-    
+
     table_length = LuaNova.raw_len(L, -1)
     @test table_length == 3
-    
+
     LuaNova.lua_pop!(L, 1)
 
-    LuaNova.close(L)    
+    LuaNova.close(L)
 
     return nothing
 end
