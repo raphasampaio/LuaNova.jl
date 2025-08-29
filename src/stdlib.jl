e2c07afd4f47ee95a5ce2978ac5e0336d9f72cf7
raphasampaio <raphaelaraujosampaio@gmail.com>
2025-08-29 15:03:04 -0300

    Update

--- src/stdlib.jl
+++ src/stdlib.jl
@@ -1,60 +1,60 @@
 function open_math_lib(L::LuaState)
-    C.luaL_requiref(L, C.LUA_MATHLIBNAME, @cfunction(C.luaopen_math, Cint, (Ptr{C.lua_State},)), 1)
-    C.lua_pop(L, 1)
+    C.luaopen_math(L)
+    C.lua_setglobal(L, C.LUA_MATHLIBNAME)
     return nothing
 end
 
 function open_string_lib(L::LuaState)
-    C.luaL_requiref(L, C.LUA_STRLIBNAME, @cfunction(C.luaopen_string, Cint, (Ptr{C.lua_State},)), 1)
-    C.lua_pop(L, 1)
+    C.luaopen_string(L)
+    C.lua_setglobal(L, C.LUA_STRLIBNAME)
     return nothing
 end
-
+   
 function open_table_lib(L::LuaState)
-    C.luaL_requiref(L, C.LUA_TABLIBNAME, @cfunction(C.luaopen_table, Cint, (Ptr{C.lua_State},)), 1)
-    C.lua_pop(L, 1)
+    C.luaopen_table(L)
+    C.lua_setglobal(L, C.LUA_TABLIBNAME)
     return nothing
 end
 
 function open_io_lib(L::LuaState)
-    C.luaL_requiref(L, C.LUA_IOLIBNAME,  @cfunction(C.luaopen_io, Cint, (Ptr{C.lua_State},)), 1)
-    C.lua_pop(L, 1)
+    C.luaopen_io(L)
+    C.lua_setglobal(L, C.LUA_IOLIBNAME)
     return nothing
 end
 
 function open_os_lib(L::LuaState)
-    C.luaL_requiref(L, C.LUA_OSLIBNAME, @cfunction(C.luaopen_os, Cint, (Ptr{C.lua_State},)), 1)
-    C.lua_pop(L, 1)
+    C.luaopen_os(L)
+    C.lua_setglobal(L, C.LUA_OSLIBNAME)
     return nothing
 end
 
 function open_utf8_lib(L::LuaState)
-    C.luaL_requiref(L, C.LUA_UTF8LIBNAME, @cfunction(C.luaopen_utf8, Cint, (Ptr{C.lua_State},)), 1)
-    C.lua_pop(L, 1)
+    C.luaopen_utf8(L)
+    C.lua_setglobal(L, C.LUA_UTF8LIBNAME)
     return nothing
 end
 
 function open_debug_lib(L::LuaState)
-    C.luaL_requiref(L, C.LUA_DBLIBNAME, @cfunction(C.luaopen_debug, Cint, (Ptr{C.lua_State},)), 1)
-    C.lua_pop(L, 1)
+    C.luaopen_debug(L)
+    C.lua_setglobal(L, C.LUA_DBLIBNAME)
     return nothing
 end
 
 function open_package_lib(L::LuaState)
-    C.luaL_requiref(L, C.LUA_LOADLIBNAME,  @cfunction(C.luaopen_package, Cint, (Ptr{C.lua_State},)), 1)
-    C.lua_pop(L, 1)
+    C.luaopen_package(L)
+    C.lua_setglobal(L, C.LUA_LOADLIBNAME)
     return nothing
 end
 
 function open_coroutine_lib(L::LuaState)
-    C.luaL_requiref(L, C.LUA_COLIBNAME, @cfunction(C.luaopen_coroutine, Cint, (Ptr{C.lua_State},)), 1)
-    C.lua_pop(L, 1)
+    C.luaopen_coroutine(L)
+    C.lua_setglobal(L, C.LUA_COLIBNAME)
     return nothing
 end
 
 function open_base_lib(L::LuaState)
-    C.luaL_requiref(L, C.LUA_GNAME, @cfunction(C.luaopen_base, Cint, (Ptr{C.lua_State},)), 1)
-    C.lua_pop(L, 1)
+    C.luaopen_base(L)
+    C.lua_setglobal(L, C.LUA_GNAME)
     return nothing
 end
 
